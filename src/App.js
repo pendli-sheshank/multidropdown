9d1eb7e14c0ecd8b810a81552737a0fde7c4d424
sheshank <sheshank3336@gmail.com>
2021-12-23 17:36:58 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import Multiselect from "multiselect-react-dropdown";
 import { useState } from "react";
+import Select from "react-select";
+import { AiOutlineLeft } from "react-icons/ai"
 import "./App.css";
 
 function App() {
   const data = [
-    { country: "India", id: 1 },
-    { country: "America", id: 2 },
-    { country: "Europe", id: 3 },
-    { country: "Germany", id: 4 },
-    { country: "Canada", id: 5 },
-    { country: "Austrila", id: 6 },
-    { country: "Italy", id: 7 },
-    { country: "Russia", id: 8 }
+    { label: "India", value: 1 },
+    { label: "America", value: 2 },
+    { label: "Europe", value: 3 },
+    { label: "Germany", value: 4 },
+    { label: "Canada", value: 5 },
+    { label: "Austrila", value: 6 },
+    { label: "Italy", value: 7 },
+    { label: "Russia", value: 8 }
   ];
   const [options] = useState(data);
+  const [displayValue, getvalue] = useState();
+  const Selectedelements = (e) => {
+    getvalue(e.map(ss => (<li>{ss.label}<AiOutlineLeft /></li>)));
+
+  }
+
   return (
     <div className="App">
       <div className="dd">
         <h3>Multiselect</h3>
-        <Multiselect options={options} displayValue="country" />
+        <Select className="select" isMulti options={data} onChange={Selectedelements} ></Select>
+
       </div>
-      <div className="dd">
-        <h1>This is side container</h1>
+      <div className="dd1">
+        <h4>This is side container</h4>
+        <ul className="col">
+          {displayValue}
+
+        </ul>
+
+
       </div>
     </div>
   );
